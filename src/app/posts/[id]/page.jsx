e1b4ad913fb972b738ab5b06f7a55cb48a228328
nayeem-miah <nayeem5113a@gmail.com>
2025-01-14 13:06:56 +0600

    du=ynamic data fetching

---
+++ src/app/posts/[id]/page.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+
+const fetchingDetails =async(id) =>{
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+    const response = await res.json()
+    return response;
+} 
+
+
+const postDetails =async ({params}) => {
+    // console.log(params.id);
+    const data = await fetchingDetails(params.id);
+    console.log(data);
+    return (
+        <div className='mt-20'>
+            post details id of : {params.id}
+            
+
+            <div className=" px-5 p-4">
+                <h2>user id of : {data.userId}</h2>
+              <h3 className="text-2xl via-violet-500 my-3">{data?.title}</h3>
+              <h3 className="text-gray-50"> {data?.body}</h3>
+            </div>
+        </div>
+    );
+};
+
+export default postDetails;
--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,4 +1,5 @@
 import getPosts from "@/Components/getPost";
+import Link from "next/link";
 import React from "react";
 
 // const getPost = async () => {
@@ -8,16 +9,18 @@ import React from "react";
 
 const postPage = async () => {
   const posts = await getPosts();
-  console.log(posts);
+//   console.log(posts);
   return (
     <div className="mt-20">
       post pages : {posts?.length}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 p-2">
+      <div  className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 p-2">
         {posts.map((post) => (
-          <div className="border border-green-500 p-4">
-            <h3 className="text-2xl via-violet-500 my-3">{post.title}</h3>
-            <h3 className="text-gray-50"> {post?.body}</h3>
-          </div>
+          <Link key={post.id} href={`/posts/${post.id}`}>
+            <div className="border border-green-500 p-4">
+              <h3 className="text-2xl via-violet-500 my-3">{post.title}</h3>
+              <h3 className="text-gray-50"> {post?.body}</h3>
+            </div>
+          </Link>
         ))}
       </div>
     </div>
