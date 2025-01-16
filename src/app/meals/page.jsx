4fd31d0d9ec2a093b8693d82104ec49a1fbd8bca
nayeem-miah <nayeem5113a@gmail.com>
2025-01-16 12:40:45 +0600

    generateMetadata dynamic meta data

--- src/app/meals/page.jsx
+++ src/app/meals/page.jsx
@@ -1,20 +1,26 @@
-import Meals from '@/Components/Meals';
-import React from 'react';
+import Meals from "@/Components/Meals";
+import React from "react";
 
+// absolute work on ignore static title
 export const metadata = {
-  title: "meals",
+  title: {
+    absolute: "meals",
+  },
   description: "meals",
 };
 const mealsPage = () => {
-    return (
-        <div className='mt-20'>
-            <h4 className='text-4xl text-green-300 '>Ali <span className='text-purple-500'> H</span>asan <span className='animate-pulse text-orange-600'>parnto  <h3 className='animate-spin'>*</h3></span></h4>
-           
-           <Meals/>
-        </div>
-    );
+  return (
+    <div className="mt-20">
+      <h4 className="text-4xl text-green-300 ">
+        Ali <span className="text-purple-500"> H</span>asan{" "}
+        <span className="animate-pulse text-orange-600">
+          parnto <h3 className="animate-spin">*</h3>
+        </span>
+      </h4>
+
+      <Meals />
+    </div>
+  );
 };
 
 export default mealsPage;
-
-
--- src/app/posts/[id]/page.jsx
+++ src/app/posts/[id]/page.jsx
@@ -1,33 +1,44 @@
-import React from 'react';
+import React from "react";
 
-
-const fetchingDetails =async(id) =>{
-   try {
+const fetchingDetails = async (id) => {
+  try {
     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
-    const response = await res.json()
+    const response = await res.json();
     return response;
-   } catch (error) {
+  } catch (error) {
     console.error(error);
-   }
-} 
+  }
+};
 
+// dynamic metadata
+export const generateMetadata = async ({ params }) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.id}`
+  );
 
-const postDetails =async ({params}) => {
-    // console.log(params.id);
-    const data = await fetchingDetails(params.id);
-    console.log(data);
-    return (
-        <div className='mt-20'>
-            post details id of : {params.id}
-            
+  const postDate = await res.json();
+  console.log(postDate);
+  return {
+    title: `${postDate?.title}`,
+    description: `${postDate?.body}`,
+    keywords: postDate?.body.split(' ')
+  };
+};
 
-            <div className=" px-5 p-4">
-                <h2>user id of : {data?.userId}</h2>
-              <h3 className="text-2xl via-violet-500 my-3">{data?.title}</h3>
-              <h3 className="text-gray-50"> {data?.body}</h3>
-            </div>
-        </div>
-    );
+const postDetails = async ({ params }) => {
+  // console.log(params.id);
+  const data = await fetchingDetails(params.id);
+  console.log(data);
+  return (
+    <div className="mt-20">
+      post details id of : {params.id}
+      <div className=" px-5 p-4">
+        <h2>user id of : {data?.userId}</h2>
+        <h3 className="text-2xl via-violet-500 my-3">{data?.title}</h3>
+        <h3 className="text-gray-50"> {data?.body}</h3>
+      </div>
+    </div>
+  );
 };
 
 export default postDetails;
